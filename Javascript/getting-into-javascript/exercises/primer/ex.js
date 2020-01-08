fd7d466c80296e128016fca6e2a8bc8faa503e19
Moumen Amin <Moumen.Amin@hotmail.com>
2020-01-08 19:18:29 +0200

    excercise solution

---
+++ Javascript/getting-into-javascript/exercises/primer/ex.js
@@ -0,0 +1,23 @@
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+  if (!bookName.toLowerCase().includes("great")) {
+    favoriteBooks.push(bookName);
+  }
+}
+// TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+  console.log(`Favourite books: ${favoriteBooks.length}`);
+  for (let book in favoriteBooks) {
+    console.log(`${book}`);
+  }
+}
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();

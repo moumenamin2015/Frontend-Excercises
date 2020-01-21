27a85f68d959b4cd66ea2f80976aaaf04917633c
Moumen Amin <Moumen.Amin@linkdev.com>
2020-01-21 15:43:35 +0200

    Master/Detail Components step

--- Javascript/Angular/Hero app/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Javascript/Angular/Hero app/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Hero } from "./../heroes/hero";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

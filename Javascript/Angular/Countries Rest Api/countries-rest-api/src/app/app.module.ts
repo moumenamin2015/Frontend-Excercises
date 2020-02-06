139ad044512b352f8c4ebbb6fa8db94aecde4f33
Moumen Amin <Moumen.Amin@linkdev.com>
2020-02-06 15:03:27 +0200

    fix some styling and adding fontawesome

--- Javascript/Angular/Countries Rest Api/countries-rest-api/src/app/app.module.ts
+++ Javascript/Angular/Countries Rest Api/countries-rest-api/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

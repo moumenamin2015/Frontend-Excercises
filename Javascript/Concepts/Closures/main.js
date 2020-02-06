c9c038e6f4e85a5893356fa7914b5cc321e24616
Moumen Amin <Moumen.Amin@linkdev.com>
2020-02-06 12:41:16 +0200

    code trials

--- Javascript/Concepts/Closures/main.js
+++ Javascript/Concepts/Closures/main.js
@@ -11,3 +11,65 @@ const c1 = increment();
 const c2 = increment();
 const c3 = increment();
 console.log("example increment", c1, c2, c3);
+
+var num = 10,
+  name = "Addy Osmani",
+  obj1 = {
+    value: "first value"
+  },
+  obj2 = {
+    value: "second value"
+  },
+  obj3 = obj2;
+
+function change(num, name, obj1, obj2) {
+  num = num * 10;
+  name = "Paul Irish";
+  obj1 = obj2;
+  obj2.value = "new value";
+}
+
+change(num, name, obj1, obj2);
+console.log(num);
+console.log(name);
+console.log(obj1.value);
+console.log(obj2.value);
+console.log(obj3.value);
+
+for (let i = 0; i < 101; i++) {
+  if (i % 3 === 0) console.log(i + "fizz");
+  else if (i % 5 === 0) console.log(i + "buzz");
+}
+
+var animals = [
+  {
+    name: "Meowsy",
+    species: "cat",
+    foods: {
+      likes: ["tuna", "catnip"],
+      dislikes: ["ham", "zucchini"]
+    }
+  },
+  {
+    name: "Barky",
+    species: "dog",
+    foods: {
+      likes: ["bones", "carrots"],
+      dislikes: ["tuna"]
+    }
+  },
+  {
+    name: "Purrpaws",
+    species: "cat",
+    foods: {
+      likes: ["mice"],
+      dislikes: ["cookies"]
+    }
+  }
+];
+var isCats = function(animal) {
+  return animal.species === "cat";
+};
+var cats = animals.filter(isCats);
+var others = animals.filter(isCats);
+console.log(cats);
